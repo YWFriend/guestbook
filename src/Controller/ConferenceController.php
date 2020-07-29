71429e97c7a507141415a40dc814bd79e28139c5
Nikita Ch <nikita.chapligin@gmail.com>
2020-07-29 13:32:56 +0300

    Add docker-compose

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,17 +3,35 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     /**
-     * @Route("/conference", name="conference")
+     * @Route("/hello/{name}", name="homepage")
+     * @param Request $request
+     * @return Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('conference/index.html.twig', [
+        /*return $this->render('conference/index.html.twig', [
             'controller_name' => 'ConferenceController',
-        ]);
+        ]);*/
+        $great = '';
+        if ($name = $request->query->get('hello')){
+            $great = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+
+        return new Response(<<<EOF
+<html>
+<body>
+$great
+<img src="/img/under-construction.gif" alt="">
+</body>
+</html>
+EOF
+        );
     }
 }
